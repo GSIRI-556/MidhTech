86431aedb9ac20fc17e4d14432c7758997ab1c6c
siri <sirigutthi@gmail.com>
2026-07-06 21:50:28 +0530

    Initial python Programs

---
+++ conditionalstatements.py
@@ -0,0 +1,48 @@
+#  Among three numbers which one is greater
+# a = int(input("Enter a number:"))  
+# b = int(input("Enter a number:")) 
+# c = int(input("Enter a number:"))
+# if a>=b and a>=c:
+#     print("a is greater")
+# elif b>=a and b>=c:
+#     print("b is greater")
+# else :
+#     print("c is greater")  
+
+# Marks = int(input("Enter your marks:"))
+# if Marks>90:
+#     print("Grade A")
+# elif Marks>60 and Marks<90:
+#     print("Grade B")
+# elif Marks>35 and Marks<60:
+#     print("Grade c")    
+# else:      
+#    print("Failed")  
+
+# Nested if else
+# age = 18
+# citizenship = True
+# if age>18:
+#     if citizenship :
+#         print("your ready for vote")
+#     else :
+#         print("you need citizenship")
+# else:
+#     print("Atleast you need 18")             
+
+#task calculator
+# operator = input("Enter an operator:")
+# num1 = int(input("Enter a number:"))
+# num2 = int(input("Enter a number:"))
+# if operator == '+':
+#     print("Addition",num1+num2)
+# elif operator == '-':
+#     print("subtraction",num1-num2)
+# elif operator == '*':
+#     print("Multlplication",num1*num2)  
+# elif operator == '/':
+#     print("Division",num1/num2)             
+# elif operator == '%':
+#     print("Modulo",num1%num2) 
+# else:
+#     print("Required operator")  
---
+++ tasks.py
@@ -0,0 +1,73 @@
+# rows = int(input("enter your rows:"))
+# cols = int(input("Enter your cols:"))
+# sym  = input("Enter the symbol:")
+# for i in range(rows):
+#     for j in range(cols):
+#         print(sym,end=" ")
+#     print()
+
+# r = int(input("enter the rows:"))
+# c = int(input("Enter the cols:"))
+# dialpad="* 0 #"
+# count = 1;
+# for i in range(r):
+#     for j in range(c):
+#          print(count,end= " ")
+#          count+=1
+#     print()
+# print(dialpad)
+
+
+# dialpad = [
+#     ["1", "2", "3"],
+#     ["4", "5", "6"],
+#     ["7", "8", "9"],
+#     ["*", "0", "#"]
+# ]
+
+# for row in dialpad:
+#     for item in row:
+#         print(item, end=" ")
+#     print()
+
+#compound interest task
+# principal = float(input("enter the principal amount: "))
+# rate = float(input("enter rate percentage: "))
+# time = int(input("enter no.of years: "))
+# amount = principal*(1+rate/100)**time
+# print("compound interest is: ",amount-principal)
+
+#random number guessing
+# import random
+# number = random.randint(1,100)
+# while True:
+#     guess = int(input("guess a number between 1 and 100: "))
+#     if guess == number:
+#         print("you guessed the number correctly")
+#         break
+#     elif guess < number:
+#         print("Too low try again!")
+#     else:
+#         print("Too high try again!")    
+
+#rock, paper, scissor game
+# import random
+# choices = ["rock", "paper", "scissors"]
+# while True:
+#     computer = random.choice(choices)
+#     user = input("Enter rock, paper, or scissors: ")
+#     if user == computer:
+#         print("It's a tie")
+#     elif(user == "rock" and computer =="scissors")or\
+#         (user == "paper" and computer =="rock")or\
+#         (user == "scissors" and computer =="paper"):
+#         print("you wins")
+#     elif user in choices:
+#        print("computer wins")
+#     else:
+#         print("choice is invalid")
+#     print("computer chose: ", computer)
+#     play = input("Play again? (yes/no): ")
+#     if play!= "yes":
+#         print("game over!")
+#         break
