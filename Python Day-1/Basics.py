f146a69dab4accfba333ac83b41d95cff8900ef8
GSIRI-556 <sirigutthi@gmail.com>
2026-07-06 22:35:31 +0530

    Add files via upload

---
+++ Python Day-1/Basics.py
@@ -0,0 +1,87 @@
+# students=20
+# amount_fee=100
+# paid_students=12
+# total=amount_fee*paid_students
+# print("Total Amount":total)
+
+#  Formatted String
+# name = "siri"
+# age = 22
+# Designation = "softwareDeveloper"
+# print(f"My name is {name}")
+# print(f" My name is {age}")
+# print(f"Designation is {Designation}")
+
+#boolean
+# username= "student"
+# log_value=False
+# if log_value:
+#    print(username)
+# else:
+#    print("No user is there")   
+
+
+# username= "student"
+# logged_out=True
+# if logged_out :
+#    print(username)
+# else:
+#    print("No user is there")   
+
+
+# student = ['Apple','orange','watermelon']
+# print("my type is",type(student))
+# age = 20
+# print("my type is",type(age))
+# percentage = 10.5
+# print("my type is",type(percentage))
+# bool = True
+# print("my type is",type(bool))
+# name = "siri"
+# print("my type is",type(name))  
+
+# change the type into boolean
+# age = 20
+# print("Actualtype is:",type(age))
+# cast = bool(age)
+# print("Type Casting:",type(cast))
+# print(cast)  
+
+
+#input method
+# n= int(input("Enter your roll number:")) 
+# name = input("Enter your name:")
+# marks= float(input("Enter your marks:")) 
+
+#rectangle 
+# width = float(input("Enter the value:"))
+# length = int(input("Enter the value:"))
+# Area = length*width 
+# print(f"area is {Area}")  
+
+# num1 = int(input("Enter a number:"))  
+# num2 = int(input("Enter a number:")) 
+# num3 = int(input("Enter a number:"))
+# Maximum = max(num1,num2,num3)
+# print("Maximum:",Maximum)
+# Minimum = min(num1,num2,num3)
+# print("Minimum:",Minimum) 
+  
+
+
+
+
+  
+
+
+
+     
+    
+
+
+
+
+
+
+
+
---
+++ Python Day-1/Operators.py
@@ -0,0 +1,67 @@
+#operators  
+# Tel = int(input("Enter your marks:"))
+# Hin = int(input("Enter your marks:"))
+# Eng = int(input("Enter your marks:"))
+# Mat = int(input("Enter your marks:"))
+# sci = int(input("Enter your marks:"))
+# soc = int(input("Enter your marks:"))
+# Total = Tel+Hin+Eng+Mat+sci+soc
+# print("Total Marks:",Total)
+# percentage = (Total/600)*100
+# print("Total Percentage:",percentage)    
+
+#Assignment operators 
+# score = 10
+# score += 5
+# print("Adding",score) 
+  
+#comparison operators
+# wallet1=50
+# wallet2=40
+# if wallet1 == wallet2 :
+#     Print("same")
+# else:
+#     print("Not same")  
+
+# n = int(input("Enter your age:"))
+# print("Accessable:",n>18)
+# if(n>18):
+#     print("Eligible for vote")
+# else:
+#     print("Not Eligible")      
+
+#Logical operators 
+# Pass_marks = 35
+# Firstclass = 80
+# Marks = int(input("Enter your marks:"))
+# if Marks>35 & Marks>Firstclass :
+#     print("Distinction")
+# elif  Marks>35 | Marks>Firstclass :
+#     print("SecondClass")
+# else :
+#     print("Fail")  
+   
+# Identity Operators
+# Basket1 = ['Apple','Orange','Grapes']
+# Basket2 = ['Guava','Grapes']
+# newBasket = Basket2
+# Basket3 = ['Guava','Grapes']
+# print("Same or not same here is :",Basket1 is Basket2) 
+# print("Same or not same here is :",newBasket is not Basket2) 
+# print("Same or not same here is :",newBasket is Basket2) # here same objects
+# print("Same or not same here is :",newBasket is Basket3) # there are two different objects
+
+#Membership Operators
+# Basket1 = ['Apple','Orange','Grapes']
+# Basket2 = 'Grapes'
+# print("IS there or not",Basket2 in Basket1)
+# print("IS there or not",Basket2 not in Basket1)  
+
+#Bit-Wise Operators 
+# Read = 1
+# Write = 2
+# Execute = 4
+# user_permission = Read | Write
+# print("can read:",bool(user_permission & Read))
+# print("can Write:",bool(user_permission & Write))
+# print("can Execute:",bool(user_permission & Execute))  
